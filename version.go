d8ded1ac2ee7cdbe17ef44c56bfd21b1850249f4
NikitaPetaichuk <pet.ai.4.uk@yandex.ru>
2021-07-05 17:22:15 +0300

    v2.1.0

--- version.go
+++ version.go
@@ -3,9 +3,13 @@ package gopackexample
 import "fmt"
 
 func PackageVersion() {
-	fmt.Println("Package version - 2.0.0")
+	fmt.Println("Package version - 2.1.0")
 }
 
 func PackageFunctionality(a, b int) int {
 	return a * b
+}
+
+func PackageFunctionality2(a, b, c int) int {
+	return a + b*c
 }
